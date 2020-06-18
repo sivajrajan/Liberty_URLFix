e65d03df43855d5ebe8224687fbaa4511c2a266b
sivajrajan <sivajrajan@gmail.com>
2020-06-18 10:40:15 +0530

    ES5 changes to IE compatibility fix

--- Liberty/botui/js/index.js
+++ Liberty/botui/js/index.js
@@ -1,9 +1,14 @@
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 var $messages = $('.messages-content');
 var serverResponse = "wala";
-var button_val = ""
-var suggession;
+var button_val = "";
+var suggession; //speech reco
 
-//speech reco
 /*
 try {
   var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -27,19 +32,18 @@ recognition.onresult = (event) => {
 
 */
 
-function listendom(no){
-  console.log(no)
-  //console.log(document.getElementById(no))
-document.getElementById("MSG").value= no.innerHTML;
+function listendom(no) {
+  console.log(no); //console.log(document.getElementById(no))
+
+  document.getElementById("MSG").value = no.innerHTML;
   insertMessage();
 }
 
-$(window).load(function() {
+$(window).load(function () {
   $messages.mCustomScrollbar();
-  setTimeout(function() {
+  setTimeout(function () {
     serverMessage("Hello. I'm your Human Capital Virtual Assistant..");
   }, 100);
-
 });
 
 function updateScrollbar() {
@@ -49,90 +53,94 @@ function updateScrollbar() {
   });
 }
 
-
-
 function insertMessage(inp_val) {
   msg = $('.message-input').val();
+
   if ($.trim(msg) == '') {
     return false;
   }
-  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-  fetchmsg(2)
 
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  fetchmsg(2);
   $('.message-input').val(null);
   updateScrollbar();
-
 }
 
 function insertMessage1(inp_val) {
   msg = inp_val;
   button_val = inp_val;
+
   if ($.trim(msg) == '') {
     return false;
   }
+
   $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-  fetchmsg(1)
+  fetchmsg(1);
 }
 
-document.getElementById("mymsg").onsubmit = (e)=>{
-  e.preventDefault()
-  insertMessage();
-  // serverMessage("hello");
+document.getElementById("mymsg").onsubmit = function (e) {
+  e.preventDefault();
+  insertMessage(); // serverMessage("hello");
   // speechSynthesis.speak( new SpeechSynthesisUtterance("hello"))
-}
+};
 
 function serverMessage(response2) {
   if ($('.message-input').val() != '') {
     return false;
   }
+
   $('<div class="message loading new"><figure class="avatar"><img src="css/bot.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
   updateScrollbar();
-  setTimeout(function() {
+  setTimeout(function () {
     $('.message.loading').remove();
     $('<div class="message new"><figure class="avatar"><img src="css/bot.png" /></figure>' + response2 + '</div>').appendTo($('.mCSB_container')).addClass('new');
     updateScrollbar();
-  }, 100 + (Math.random() * 20) * 100);
-
+  }, 100 + Math.random() * 20 * 100);
 }
 
+function fetchmsg(butt) {
+  var url = 'http://localhost:5000/send-msg';
+  var data = new URLSearchParams();
 
+  if (butt == 2) {
+    var _iterator = _createForOfIteratorHelper(new FormData(document.getElementById("mymsg"))),
+        _step;
 
-function fetchmsg(butt){
-
-     var url = 'http://localhost:5000/send-msg';
-     const data = new URLSearchParams();
-	 
-      if (butt == 2){
-        for (const pair of new FormData(document.getElementById("mymsg"))) {
-            data.append(pair[0], pair[1]);
-        }
-      }else{
-        data.append('MSG', button_val);
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var pair = _step.value;
+        data.append(pair[0], pair[1]);
       }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+  } else {
+    data.append('MSG', button_val);
+  }
 
-      console.log("abc",data)
-        fetch(url, {
-          method: 'POST',
-          body:data
-        }).then(res => res.json())
-         .then(response => {
-          console.log(response);
-         serverMessage(response.Reply);
-          //speechSynthesis.speak( new SpeechSynthesisUtterance(response.Reply))
-         })
-          .catch(error => console.error('Error h:', error));
-
+  console.log("abc", data);
+  fetch(url, {
+    method: 'POST',
+    body: data
+  }).then(function (res) {
+    return res.json();
+  }).then(function (response) {
+    console.log(response);
+    serverMessage(response.Reply); //speechSynthesis.speak( new SpeechSynthesisUtterance(response.Reply))
+  }).catch(function (error) {
+    return console.error('Error h:', error);
+  });
 }
 
-$(function() {
-  $("#chat-circle").click(function() {
+$(function () {
+  $("#chat-circle").click(function () {
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
-  })
-
-  $(".chat-box-toggle").click(function() {
+  });
+  $(".chat-box-toggle").click(function () {
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
-  })
-
-})
+  });
+});
